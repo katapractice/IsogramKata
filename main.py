a702ce907669cfc0e9eb442b1b884d23255f2d42
Nick Haynes <nicholashaynes1@gmail.com>
2019-08-16 11:51:00 -0600

    First solution.
    The method takes in the string iterates through every word and prints the repeating lettters.

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 #	@Author: Nick Haynes
 def is_isogram(string):
-	
+	for char in string:
+		if string.count(char) == 1:
+			print(char + " Doesn't repeat")
+
+		else:
+			print("The repeating letter is " + char)
+
+
+
+is_isogram("Hello")
