fd5812e54928ee35783aebf1b35ef4da40239ace
Nick Haynes <nicholashaynes1@gmail.com>
2019-08-16 12:00:03 -0600

    Added the returns.
    When the word has repeating letters the code will exit.

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 #	@Author: Nick Haynes
 def is_isogram(string):
+	string = string.lower()
 	for char in string:
 		if string.count(char) == 1:
 			print(char + " Doesn't repeat")
 
 		else:
 			print("The repeating letter is " + char)
+			return False
+
+	return True
 
 
-
-is_isogram("Hello")
+print(is_isogram("abc"))
